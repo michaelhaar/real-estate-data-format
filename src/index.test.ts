cc9676e30de7776497208b98c4ae7bbf16d33f4f
Michael Haar <michael.haar92@gmail.com>
2024-03-16 18:24:18 +0100

    include example in Readme

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+import path from "path";
 import { test, expect, describe } from "vitest";
 import Ajv, { JSONSchemaType } from "ajv";
 import addFormats from "ajv-formats";
@@ -7,67 +9,20 @@ const ajv = new Ajv({ removeAdditional: "all" });
 addFormats(ajv);
 const validate = ajv.compile(realEstateListingSchema as JSONSchemaType<RealEstateListing>);
 
-const realEstateListing = {
-  listingId: "<string>",
-  url: "<string>",
-  title: "<string>",
-  description: "<string>",
-  images: ["<string>"],
-  propertyType: "<string>",
-  address: {
-    street: "<string>",
-    city: "<string>",
-    state: "<string>",
-    postalCode: "<string>",
-    country: "<string>",
-    countryCode: "<string>",
-  },
-  coordinates: {
-    latitude: 90,
-    longitude: 92,
-  },
-  monetaryDetails: {
-    purchasingPrice: 243,
-    rent: 266,
-    currencyCode: "<string>",
-    isCommissionFree: true,
-    estMonthlyOperatingCosts: 182,
-  },
-  features: {
-    livingArea: 247,
-    plotArea: 185,
-    yearBuilt: 2022,
-    bedrooms: 23,
-    bathrooms: 24,
-    hasGarage: true,
-    hasCarport: true,
-    hasParkingSpace: true,
-    hasTerrace: true,
-    hasBalcony: true,
-    hasGarden: true,
-    hasLoggia: true,
-    hasPool: true,
-    hasStorageRoom: true,
-    isBarrierFree: true,
-    hasBuiltInKitchen: true,
-    hasElevator: true,
-    hasAirConditioning: true,
-    hasBasementCompartment: true,
-  },
-  contactDetails: {
-    name: "<string>",
-    email: "test@gmail.com",
-    phone: "<string>",
-  },
-  snapshotDate: "2022-03-14T01:59:59Z",
-  scrapedFrom: "<string>",
-  publishedAt: "2022-03-14T01:59:59Z",
-  updatedAt: "2022-03-14T01:59:59Z",
-};
+function getExampleJsonFromReadme() {
+  const readme = fs.readFileSync(path.resolve(__dirname, "../README.md"), "utf8");
+  const example = readme.match(/```json\n([\s\S]+?)```/)?.[1];
+  if (!example) {
+    throw new Error("No JSON example found in README");
+  }
+  return JSON.parse(example);
+}
 
-test("should pass", () => {
-  const valid = validate(realEstateListing);
-  expect(valid).toBe(true);
+const realEstateListing = getExampleJsonFromReadme();
+
+test("Example JSON from README should pass", () => {
+  validate(realEstateListing);
+  expect(validate.errors).toBeFalsy();
 });
 
 describe("should remove additional properties", () => {
